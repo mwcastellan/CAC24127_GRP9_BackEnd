de6eed618b31d8964aedb2bdb20083f9a1d4d216
mcastellan1 <101480046+mcastellan1@users.noreply.github.com>
2024-06-15 20:55:22 -0300

    Marcelo W. Castellan

--- controllers/pedidosControllers.js
+++ controllers/pedidosControllers.js
@@ -5,7 +5,7 @@ res.send ("Busco los pedidos de la BD y los envio en .JSON")
 }
 
 const traerUnPedido = (req,res) =>{
-    res.send ("busco el pedido de la BD y lo envio en .JSON")
+    res.send ("Busco el pedido de la BD y lo envio en .JSON")
     }
 
     module.exports = {traerPedidos,traerUnPedido}
