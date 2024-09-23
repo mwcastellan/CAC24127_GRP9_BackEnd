331d1399c29c5659f56196125543d022175191ca
mcastellan1 <101480046+mcastellan1@users.noreply.github.com>
2024-09-22 22:43:02 -0300

    Marcelo W. Castellan

--- routes/pedidosRouter.js
+++ routes/pedidosRouter.js
@@ -15,23 +15,25 @@ const {
 
 const config = require("../config");
 const ValidarPedido = require("../controllers/pedidosValidator.js");
+const {
+  GrabarLogs_procesos,
+} = require("../controllers/logs_procesosControllers.js");
 
 fechahora = new Date();
 
 //---------------------------------------------------------------------------------
 // Autorizar Cliente -- Valida con datos en la cookie tpo-nodejs-bb
 const AutorizarClientes = (req, res, next) => {
-  const Grabarlogs_Procesos = require("../controllers/logs_procesosControllers.js");
   try {
-    Grabarlogs_Procesos("Autorizar Cliente: 1- req.cookies: ");
+    GrabarLogs_procesos("Autorizar Cliente: 1- req.cookies: ");
     // Nombre de la cookie: tpo_nodejs_bb
-    const token = req.cookies.tpo_nodejs_bb; 
+    const token = req.cookies.tpo_nodejs_bb;
     try {
-      Grabarlogs_Procesos("Autorizar Cliente: 2- Token : " + token);
+      GrabarLogs_procesos("Autorizar Cliente: 2- Token : " + token);
       const data = jwt.verify(token, config.tokensJWT.secretKey);
       req.body.IDCLIENTE = data.IDCLIENTE;
       req.body.EMAIL = data.EMAIL;
-      Grabarlogs_Procesos(
+      GrabarLogs_procesos(
         "Autorizar Cliente: 3- Autotizado : " +
           req.body.EMAIL +
           " - IDCLIENTE : " +
@@ -39,11 +41,11 @@ const AutorizarClientes = (req, res, next) => {
       );
       next();
     } catch (error) {
-      Grabarlogs_Procesos("Autorizar Cliente: 4- Error jwt.verify : " + error);
+      GrabarLogs_procesos("Autorizar Cliente: 4- Error jwt.verify : " + error);
       res.body = res.status(419).json({ message: [{ msg: error.message }] });
     }
   } catch (error) {
-    Grabarlogs_Procesos("Autorizar Cliente: 5- Error req.cookies : " + error);
+    GrabarLogs_procesos("Autorizar Cliente: 5- Error req.cookies : " + error);
     res.body = res.status(420).json({ message: [{ msg: error.message }] });
   }
 };
